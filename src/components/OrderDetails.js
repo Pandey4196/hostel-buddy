4fbbb7337231f1ae21efb46a4337408851779374
Pandey4196 <anushkpandey@gmail.com>
2022-08-02 08:16:36 +0530

    Complete order details page and signup page

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -9,17 +9,23 @@ export default function OrderDetails() {
             <Template />
             <div className="signup-bg-box">
                 <h3 className="order-details-header">ORDER DETAILS</h3>
-                <div className="input-box-1">
-                <input  placeholder="Product Name" type="text" className="input-box-2" name="name" id="name" />
+                <div className="input-box-3">
+                <input  placeholder="Product Name" type="text" className="input-box-2" name="name" id="prdctname" />
                 </div>
-                <div className="input-box-1">
-                <input placeholder="Pickup Location" type="text" className="input-box-2" name="username" id="username" />
+                <div className="input-box-3">
+                <input  placeholder="Payment Status" type="text" className="input-box-2" name="name" id="payment" />
+                </div> 
+                <div className="input-box-3">
+                <input placeholder="Pickup Location" type="text" className="input-box-2" name="username" id="location" />
                 </div>
-                <div className="input-box-1">
-                <input placeholder="Hostel Block" type="text" className="input-box-2" name="regno" id="regno" />
+                <div className="input-box-3">
+                <input placeholder="Hostel Block" type="text" className="input-box-2" name="hostel" id="hostel-block" />
                 </div>
-                <div className="input-box-1">
-                <input placeholder="Email" type="text" className="input-box-2" name="hostel block" id="hostel-block" />
+                <div className="input-box-3">
+                <input placeholder="Email" type="text" className="input-box-2" name="hostel block" id="email" />
+                </div>
+                <div className="input-box-3">
+                <input placeholder="Contact Details" type="text" className="input-box-2" name="hostel block" id="contact" />
                 </div>
                 <div>
                 <button type="button" className="submit-button-signup" id="submit">SUBMIT</button>
